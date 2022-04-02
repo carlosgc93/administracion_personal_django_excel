2f079213a22e35f93587b66dac6b8a56b002e15a
Carlos Garcia <carlosgc93@users.noreply.github.com>
2022-04-01 19:34:42 -0600

    Update settings.py
    SK

--- INM_proyecto/settings.py
+++ INM_proyecto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2c_9koj(xds*vk@07j#bdlpm59ex!fhq1)1)j!wt@-k*ps2qe8'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
